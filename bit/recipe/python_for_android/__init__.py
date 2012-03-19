364b5608c926717222529a1271d1a6e742865a5b
Ryan Northey <ryan@3ca.org.uk>
2012-03-19 12:04:05 +0000

    clearing up

--- bit/recipe/python_for_android/__init__.py
+++ bit/recipe/python_for_android/__init__.py
@@ -6,28 +6,23 @@ import commands
 
 class Recipe:
     def __init__(self, buildout, name, options):
+        self.name = name
         self.options = options
         self.logger = logging.getLogger(name)
 
     def _update(self):
-        apis = self.options['apis'].split('\n')        
-        sdk = pkg_resources.resource_filename(__name__, 'install_python_for_android')
+        install = pkg_resources.resource_filename(__name__, 'python_for_android')
         path = os.getcwd()
-        if 'install_python_for_android' in os.listdir('bin'):
-            os.unlink('bin/install_python_for_android')
-        target_install_python_for_android = os.path.join('bin','install_python_for_android')
-        open(target_install_python_for_android,'w').write('#/bin/bash\nAPIS=%s\n' %(apis)+open(install_python_for_android).read())
-        commands.getoutput('chmod +x %s' %target_install_python_for_android)
-
+        target_install = os.path.join('bin',self.name)
+        if self.name in os.listdir('bin'):
+            os.unlink(target_install)
+        open(target_install,'w').write('#/bin/bash\nGIT_SRC="%s"\nRECIPES="%s"\nPROJECT=%s' %(self.options['src'],self.options['recipes'],self.name)+open(install).read())
+        commands.getoutput('chmod +x %s' %target_install)        
+            
     def install(self):
-        self.logger.info("Setting up %s install_python_for_android" % install_python_for_android_type)
         self._update()
-        commands.getoutput('./bin/install_python_for_android')
-        return ['bin/install_python_for_android']
+        return ['bin/%s'%self.name]
 
     def update(self):
-        command = self.options.get('update-command')
         self._update()
-        if command is not None:
-            self.logger.info("Running %s" % command)
 
