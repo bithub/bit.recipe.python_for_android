5e9c27704984f5f8bc1e83a977cd21ddc65d7c6f
Ryan Northey <ryan@3ca.org.uk>
2012-03-19 18:49:08 +0000

    pep8/pyflakes and tidying up

--- bit/recipe/python_for_android/__init__.py
+++ bit/recipe/python_for_android/__init__.py
@@ -1,44 +1,53 @@
 import logging
 import os
-import shutil
 import pkg_resources
 import commands
 
+
 class Recipe:
+
     def __init__(self, buildout, name, options):
         self.name = name
         self.options = options
         self.logger = logging.getLogger(name)
 
     def _update(self):
-        install = pkg_resources.resource_filename(__name__, 'python_for_android')
+        install = pkg_resources.resource_filename(
+            __name__, 'python_for_android')
         path = os.getcwd()
-        part = os.path.join(path,'parts',self.name)
-        target_install = os.path.join('bin',self.name)
-        sdk = os.path.join(path,self.options['sdk'])
+        target_install = os.path.join('bin', self.name)
+        sdk = os.path.join(path, self.options['sdk'])
         ndk = os.path.join(path, self.options['ndk'])
         api = self.options['api']
         ndk_version = self.options['ndk_version']
         if self.name in os.listdir('bin'):
             os.unlink(target_install)
-        defined =  '\nBUILDOUT=%s\nexport ANDROIDSDK=%s\nexport ANDROIDNDK=%s\nexport ANDROIDNDKVER=%s\nexport ANDROIDAPI=%s\nexport PATH=$ANDROIDSDK/tools:$ANDROIDNDK:$PATH\nGIT_SRC="%s"\nRECIPES="%s"\nPROJECT=%s' %(path,sdk,ndk,ndk_version,api,self.options['src'],self.options['recipes'],self.name)
-
         o = self.options
-        defined += '\nPACKAGE=%s\nAPPNAME=%s\nVERSION=%s\nORIENTATION=%s\nPERMISSIONS="%s"\nPRIVATE=%s\nPUBLIC=%s\n' % (o['package']
-                                                                                                                      ,self.name
-                                                                                                                      ,o['version']
-                                                                                                                      ,o['orientation']
-                                                                                                                      ,o['permissions']
-                                                                                                                      ,os.path.join(path,o['private'])
-                                                                                                                      ,os.path.join(path,o['public']))
-                                                                                                                      
-        open(target_install,'w').write(('#/bin/bash %s' %defined)+open(install).read())
-        commands.getoutput('chmod +x %s' %target_install)        
-            
+        env_vars = {'BUILDOUT': path,
+                    'ANDROIDSDK': sdk,
+                    'ANDROIDNDK': ndk,
+                    'ANDROIDNDKVER': ndk_version,
+                    'ANDROIDAPI': api,
+                    'GIT_SRC': o['src'],
+                    'RECIPES': o['recipes'],
+                    'PROJECT': self.name,
+                    'PACKAGE': o['package'],
+                    'APPNAME': self.name,
+                    'VERSION': o['version'],
+                    'ORIENTATION': o['orientation'],
+                    'PERMISSIONS': o['permissions'],
+                    'PRIVATE': o['private'],
+                    'PUBLIC': o['public'],
+                    }
+        bash = '#/bin/bash\n'
+        bash += '\n'.join(['%s=%s' % (k, v)
+                          for k, v in env_vars.items()])
+        open(target_install, 'w').write(bash + open(install).read())
+        commands.getoutput('chmod +x %s' % target_install)
+
     def install(self):
         self._update()
-        return ['bin/%s'%self.name]
+        return ['bin/%s' % self.name]
 
     def update(self):
         self._update()
-
--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 name = "bit.recipe.python_for_android"
 version = '0.0.1'
 setup(
-    name = name,
-    version = version,
-    author = "Ryan Northey",
-    author_email = "ryan@3ca.org.uk",
-    description = "bitfoundation python_for_android recipe",
-    license = "GPL",
-    keywords = "buildout",
-    url='http://www.python.org/pypi/'+name,
+    name=name,
+    version=version,
+    author="Ryan Northey",
+    author_email="ryan@3ca.org.uk",
+    description="bitfoundation python_for_android recipe",
+    license="GPL",
+    keywords="buildout",
+    url='http://www.python.org/pypi/' + name,
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['bit','bit.recipe'],
+    namespace_packages=['bit', 'bit.recipe'],
     include_package_data=True,
     zip_safe=False,
-    install_requires = ['zc.buildout', 'setuptools'],
-    entry_points = {'zc.buildout':
-                    ['default = %s:Recipe' % name]},
+    install_requires=['zc.buildout', 'setuptools'],
+    entry_points={'zc.buildout':
+                    ['default=%s:Recipe' % name]},
     )
